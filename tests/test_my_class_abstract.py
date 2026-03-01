9cd6e6c1cb3c0a2e588c97d486cddb9de9423a50
Alessandra Bilardi <alessandra.bilardi@gmail.com>
2026-03-01 21:10:49 +0100

    perf: changed MyClassAbstract unittests in pytest

--- tests/test_my_class_abstract.py
+++ tests/test_my_class_abstract.py
@@ -9,27 +9,11 @@ so missing the method for testing the other method.
 # see https://github.com/bilardi/python-prototype for details
 """
 
-import unittest
+import pytest
 from simple_sample.myClassAbstract import MyClassAbstract
 
-class TestMyClassAbstract(unittest.TestCase):
-    """
-    This is a basic unit test class.
-    It is not possible to instantiate a class with an abstract method,
-    so missing the method for testing the other method.
-    """
-    def __init__(self, *args, **kwargs):
-        """
-        Initialization of variables
-        """
-        unittest.TestCase.__init__(self, *args, **kwargs)
 
-    def test_my_class_abstract_can_be_created(self):
-        """
-        Verifies if the class MyClassAbstract raises an exception
-        """
-        with self.assertRaises(TypeError):
-            MyClassAbstract()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_my_class_abstract_can_be_created():
+    """Verifies if the class MyClassAbstract raises an exception"""
+    with pytest.raises(TypeError):
+        MyClassAbstract()
