c0933f8aae01d0a2cb9792e5bd9f3f16bf35b974
Alessandra Bilardi <alessandra.bilardi@gmail.com>
2026-03-01 21:10:06 +0100

    perf: changed MyClassInterface unittests in pytest

--- tests/test_my_class_interface.py
+++ tests/test_my_class_interface.py
@@ -8,41 +8,26 @@ If the functions contained conditions, there would be more tests for each public
 # see https://github.com/bilardi/python-prototype for details
 """
 
-import unittest
+import pytest
 from simple_sample.myClassInterface import MyClassInterface
 
-class TestMyClassInterface(unittest.TestCase, MyClassInterface):
-    """
-    This is a basic unit test class. There is a test for each public function.
-    If the functions contained conditions, there would be more tests for each public function.
-    """
-    # mci(MyClassInterface): a class variable with default None
-    mci = None
-    def __init__(self, *args, **kwargs):
-        """
-        Initialization of variables
-        """
-        self.mci = MyClassInterface()
-        unittest.TestCase.__init__(self, *args, **kwargs)
 
-    def test_my_class_interface_can_be_created(self):
-        """
-        Verifies if the class MyClassInterface can be created
-        """
-        self.assertTrue(isinstance(self.mci, MyClassInterface))
+@pytest.fixture
+def mci():
+    return MyClassInterface()
 
-    def test_my_class_interface_gets_bar_value(self):
-        """
-        Verifies if the class MyClassInterface bar method return None
-        """
-        self.assertIsNone(self.mci.bar())
 
-    def test_my_class_interface_gets_qux_value(self):
-        """
-        Verifies if the class MyClassInterface qux method raises an exception
-        """
-        with self.assertRaises(NotImplementedError):
-            self.mci.qux()
+def test_my_class_interface_can_be_created(mci):
+    """Verifies if the class MyClassInterface can be created"""
+    assert isinstance(mci, MyClassInterface)
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_my_class_interface_gets_bar_value(mci):
+    """Verifies if the class MyClassInterface bar method return None"""
+    assert mci.bar() is None
+
+
+def test_my_class_interface_gets_qux_value(mci):
+    """Verifies if the class MyClassInterface qux method raises an exception"""
+    with pytest.raises(NotImplementedError):
+        mci.qux()
